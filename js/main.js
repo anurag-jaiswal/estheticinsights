7a6012bf7a553a218ef4730a5a279f0081f21db3
anuragjai <14mcmc08@gmail.com>
2018-11-20 17:59:35 +0530

    Tue

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-$( document ).ready(function() {
+// $( document ).ready(function() {
  
-		$(function() {
-		  $('a[href*=#]').on('click', function(e) {
-		    e.preventDefault();
-		    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-		  });
-		});
+// 		$(function() {
+// 		  $('a[href*=#]').on('click', function(e) {
+// 		    e.preventDefault();
+// 		    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+// 		  });
+// 		});
 
-});
+// });
 
 
 //  Animation 
